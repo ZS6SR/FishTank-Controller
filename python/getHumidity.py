b53dc8ff1e64fde089eb94d717c367bb75f5973d
Sean Riley <zs6sr@acidhawk.co.za>
2019-01-31 08:22:28 +0200

    Create getHumidity.py
    Get the Humidity value and update the rrd database with it.  This is also added to cron

---
+++ python/getHumidity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import Adafruit_DHT as dht
+import rrdtool
+
+temp_db = "/home/pi/nodetests/ftc/public/rrd/humidity.rrd"
+
+h,t = dht.read_retry(dht.DHT22, 5)
+
+rrdtool.update(temp_db, 'N:%s' %(h))
+
+#print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
