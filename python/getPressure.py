eb1ae558d07777edc0f14b4f5ff8b0d66856203f
Sean Riley <zs6sr@acidhawk.co.za>
2019-01-31 08:22:37 +0200

    Create getPressure.py
    Get the Pressure value and update the rrd database with it.  This is also added to cron

---
+++ python/getPressure.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import Adafruit_BMP.BMP085 as BMP085 
+import rrdtool
+
+temp_db = "/home/pi/nodetests/ftc/public/rrd/pressure.rrd"
+
+bmp = BMP085.BMP085(mode=BMP085.BMP085_HIGHRES)
+ 
+realPress = bmp.read_pressure()
+altitude = bmp.read_altitude()
+
+#Cacluate the actual pressure becausewe are at altitude
+mslPress = realPress / pow(1-(altitude/44330), 5.257)
+
+#print "realPress  = %.2f" % realPress
+#print "mslPress   = %.2f" % mslPress
+ 
+#print "Temperature     : %.2f C" % temp
+#print "Actual Pressure : %.2f hPa" % (realPress / 100.0)
+#print "MSL Pressure    : %.2f hpa" % (mslPress / 100.0)
+#print "Altitude        : %.2f" % altitude
+
+rrdtool.update(temp_db, 'N:%.2f:%.2f' %((realPress / 100), (mslPress / 100)))
