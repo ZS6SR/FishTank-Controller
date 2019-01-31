1e644e6148a28c24169c32a239260fd2925045fb
Sean Riley <zs6sr@acidhawk.co.za>
2019-01-31 08:22:58 +0200

    Create getRoomTemps.py
    Get the AM2302 and the BMP085 values and update the rrd database with it.  This is also added to cron

---
+++ python/getRoomTemps.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import Adafruit_BMP.BMP085 as BMP085
+import Adafruit_DHT as dht
+import rrdtool
+
+temp_db = "/home/pi/nodetests/ftc/public/rrd/roomTemps.rrd"
+
+bmp = BMP085.BMP085(mode=BMP085.BMP085_HIGHRES)
+
+h,t = dht.read_retry(dht.DHT22, 5)
+temp = bmp.read_temperature()
+
+#print "AM2302 Temp: %.2f" %(t)
+#print "BMP085 Temp: %.2f" %(temp)
+
+rrdtool.update(temp_db, 'N:%s:%s' %(t, temp))
