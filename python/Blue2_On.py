e4b084f84aaa4536210a7ce35977e5dbeb0ebe8c
Sean Riley <zs6sr@acidhawk.co.za>
2019-01-31 08:19:55 +0200

    Create Blue2_On.py
    Script to turn on / off the lights that I schedule using cron

---
+++ python/Blue2_On.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+
+ledPin = 6 # Pin for Blue (2)
+
+GPIO.setwarnings(False)
+# Pin Setup:
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+
+GPIO.output(ledPin, GPIO.HIGH);  #Turn on the White1 LED light
+
