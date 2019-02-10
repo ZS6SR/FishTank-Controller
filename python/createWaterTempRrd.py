5f3ddb528bb2ddf0d70f4c41c5da5d2f5dd2a55f
Sean Riley <zs6sr@acidhawk.co.za>
2019-02-10 21:12:35 +0200

    Add files via upload
    Added an Events page where I can capture events an water conditions and insert them into a SQLite3 db

---
+++ python/createWaterTempRrd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+import rrdtool
+
+ret = rrdtool.create("waterTemp.rrd", "--step", "300", "--start", '0',
+ "DS:watertemp:GAUGE:600:U:U",
+ "RRA:AVERAGE:0.5:1:576",
+ "RRA:AVERAGE:0.5:6:672",
+ "RRA:AVERAGE:0.5:24:732", 
+ "RRA:AVERAGE:0.5:144:1460")
+
+# 2 days of 5 mins averages
+# 2 Weeks of 1/2 hour averages
+# 2 Months of 2 hour averages
+# 2 years of 12 hour averages
+
+if ret:
+ print rrdtool.error()
