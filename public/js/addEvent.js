c6d198a3be3239955aafcfc886d7ef161c8dcae7
Sean Riley <zs6sr@acidhawk.co.za>
2019-02-12 10:27:46 +0200

    Delete createWaterTemprrd.py

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+=======
+var dt   = new Date();
+var day  = ("0"+dt.getDate()).slice(-2);
+var mnth = ("0"+(dt.getMonth()+1)).slice(-2);
+var year = dt.getFullYear();
+var hour = ("0"+dt.getHours()).slice(-2);
+var mins  = ("0"+dt.getMinutes()).slice(-2);
+
+//document.getElementById('time').innerHTML=day+"-"+mnth+"-"+year+" "+hour+":"+mins;
+>>>>>>> 8670af1111bc52f5c0643143697aa0ce6fbbde80
 
 //Make connection
 var socket	= io.connect("http://192.168.100.50:8080");
@@ -5,6 +16,7 @@ var eventDateField = document.getElementById("eventDate");
 var eventTimeField = document.getElementById("eventTime");
 var currentWaterTempTxtField = document.getElementById("eventWaterTemp");
 
+<<<<<<< HEAD
 setInterval(function() {
 	var dt   = new Date();
 	var day  = ("0"+dt.getDate()).slice(-2);
@@ -16,6 +28,11 @@ setInterval(function() {
 	eventDateField.value = day+"-"+mnth+"-"+year;
 	eventTimeField.value = hour+":"+mins;
 }, 1000);
+=======
+eventDateField.value = day+"-"+mnth+"-"+year;
+eventTimeField.value = hour+":"+mins;
+
+>>>>>>> 8670af1111bc52f5c0643143697aa0ce6fbbde80
 
 socket.on('currentWaterTemp',function(data){
 	currentWaterTempTxtField.value = data;
