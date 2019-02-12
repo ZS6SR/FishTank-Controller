501d8c65d2a09e27e03cb89b6410225de6b83157
Sean Riley <zs6sr@acidhawk.co.za>
2019-02-12 10:22:22 +0200

    Update addEvent.js
    Cleaned up script

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -1,8 +1,22 @@
-var dt   = new Date();
-var day  = ("0"+dt.getDay()).slice(-2);
-var mnth = ("0"+(dt.getMonth()+1)).slice(-2);
-var year = dt.getFullYear();
-var hour = ("0"+dt.getHours()).slice(-2);
-var mins  = ("0"+dt.getMinutes()).slice(-2);
 
-document.getElementById('time').innerHTML=day+"-"+mnth+"-"+year+" "+hour+":"+mins;
+//Make connection
+var socket	= io.connect("http://192.168.100.50:8080");
+var eventDateField = document.getElementById("eventDate");
+var eventTimeField = document.getElementById("eventTime");
+var currentWaterTempTxtField = document.getElementById("eventWaterTemp");
+
+setInterval(function() {
+	var dt   = new Date();
+	var day  = ("0"+dt.getDate()).slice(-2);
+	var mnth = ("0"+(dt.getMonth()+1)).slice(-2);
+	var year = dt.getFullYear();
+	var hour = ("0"+dt.getHours()).slice(-2);
+	var mins  = ("0"+dt.getMinutes()).slice(-2);
+
+	eventDateField.value = day+"-"+mnth+"-"+year;
+	eventTimeField.value = hour+":"+mins;
+}, 1000);
+
+socket.on('currentWaterTemp',function(data){
+	currentWaterTempTxtField.value = data;
+});
